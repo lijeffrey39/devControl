501cd7b3acd7a4285bd7ad7c6263e0f061a5cbdd
Jeffrey Li <>
2018-07-16 15:08:16 -0700

    added firebase

--- newDev/server.js
+++ newDev/server.js
@@ -1,8 +1,19 @@
 var express = require('express');
+var firebase = require('firebase');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
+
+var config = {
+    apiKey: "AIzaSyAyXT6uOdnaUbVFFiFgrx15cRVqXlg36U8",
+    authDomain: "devuirequests.firebaseapp.com",
+    databaseURL: "https://devuirequests.firebaseio.com",
+    projectId: "devuirequests",
+    storageBucket: "devuirequests.appspot.com",
+    messagingSenderId: "998440297406"
+  };
+firebase.initializeApp(config);
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
@@ -21,8 +32,43 @@ router.get("/",function(req,res){
   res.render("index");
 });
 
+
+function test(val) {
+  console.log(val);
+}
+
+
 router.get("/waferStatusExample",function(req,res){
-    res.sendFile(path.join(__dirname, '') + "/waferStatusExample.html");
+
+  var adding = firebase.database().ref('/requests');
+  console.log(adding);
+
+  adding.once("value")
+    .then(function(snapshot) {
+      var name = snapshot.child("1").val(); // {first:"Ada",last:"Lovelace"}
+      console.log(name)
+      // var firstName = snapshot.child("name/first").val(); // "Ada"
+      // var lastName = snapshot.child("name").child("last").val(); // "Lovelace"
+      // var age = snapshot.child("age").val(); // null
+    });
+
+  // adding.on('value', function(snapshot) {
+  //   adding.once('value', function(snapshot) {
+  //     snapshot.forEach(function(childSnapshot) {
+  //       var childKey = childSnapshot.key;
+  //       var childData = childSnapshot.val();
+  //       console.log("hi")
+  //       if (childKey == "requests") {
+  //         test(childData);
+  //       }
+  //     });
+  //   });
+  // });
+
+
+  var hash = req.param('hash');
+  console.log(hash)
+  res.render('waferStatusExample', {hash: hash});
 });
 
 
